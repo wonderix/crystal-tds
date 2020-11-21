a5dd4ff6dcb4dec111c8354a8b2ac90bd4ce3ac0
wonderix <wonderix@googlemail.com>
2020-11-21 17:15:56 +0100

    Add decimal

---
+++ src/tds/trace.cr
@@ -0,0 +1,27 @@
+require "./utf16_io.cr"
+require "./errno.cr"
+
+module TDS::Trace
+  {% if flag?(:trace) %}
+    INDENT = Atomic.new(0)
+  {% end %}
+
+  macro trace(value)
+    {% if flag?(:trace) %}
+    puts " #{ " " * TDS::Trace::INDENT.get}{{value}} = #{ {{value}}.inspect }"
+    {{ value }}
+    {% end %}
+  end
+
+  macro trace_push
+    {% if flag?(:trace) %}
+    TDS::Trace::INDENT.add(2)
+    {% end %}
+  end
+
+  macro trace_pop
+    {% if flag?(:trace) %}
+    TDS::Trace::INDENT.sub(2)
+    {% end %}
+  end
+end
