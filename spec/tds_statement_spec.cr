3ac27ed19719ffbfdc58d6714acd4730024dfb54
wonderix <wonderix@googlemail.com>
2020-11-24 07:12:16 +0100

    Add escaping of args

--- spec/tds_statement_spec.cr
+++ spec/tds_statement_spec.cr
@@ -1,7 +1,10 @@
 require "./spec_helper"
 
 describe TDS::Statement do
-  # it "prepares statement" do
-  #   DATABASE.query_one "SELECT CAST(? as INT)", 1 { |rs| rs.read(Int32) }.should eq 1
-  # end
+  it "handles ints" do
+    DATABASE.query_one "SELECT CAST(? as INT)", 1 { |rs| rs.read(Int32) }.should eq 1
+  end
+  it "handles strings" do
+    DATABASE.query_one "SELECT ?", "'CREATE TABLE" { |rs| rs.read(String) }.should eq "'CREATE TABLE"
+  end
 end
--- src/tds/statement.cr
+++ src/tds/statement.cr
@@ -1,4 +1,5 @@
 require "./utf16_io"
+require "./decoder"
 
 class TDS::Statement < DB::Statement
   ENCODING = IO::ByteFormat::LittleEndian
@@ -7,10 +8,39 @@ class TDS::Statement < DB::Statement
     super(connection, command)
   end
 
+  protected def self.encode(value : Int8 | Int16 | Int32 | Int64 | UInt8 | UInt16 | UInt32 | UInt64 | Float64 | Float32 | BigDecimal)
+    value.to_s
+  end
+
+  protected def self.encode(value : String)
+    "'#{value.gsub(/'/, "''")}'"
+  end
+
+  protected def self.encode(value : Time)
+    "'#{Time::Format::RFC_3339.format(value)}'"
+  end
+
+  protected def self.encode(value : Nil)
+    "NULL"
+  end
+
+  private def expanded_command(args : Enumerable)
+    index = -1
+    cmd = command.gsub(/\?/) do |s|
+      begin
+        index += 1
+        Statement.encode(args[index])
+      rescue ::IndexError
+        raise DB::Error.new("To few arguments for statement #{command}")
+      end
+    end
+    raise DB::Error.new("To much arguments for statement #{command}") if index != args.size - 1
+    cmd
+  end
+
   protected def perform_query(args : Enumerable) : DB::ResultSet
-    raise NotImplemented.new("prepared statements are not supported yet") unless args.empty?
     connection.send(PacketIO::Type::QUERY) do |io|
-      UTF16_IO.write(io, command, ENCODING)
+      UTF16_IO.write(io, expanded_command(args), ENCODING)
     end
     result = nil
     connection.recv(PacketIO::Type::REPLY) do |io|
@@ -23,7 +53,7 @@ class TDS::Statement < DB::Statement
     rows_affected : Int64 = 0
     last_id : Int64 = 0
     connection.send(PacketIO::Type::QUERY) do |io|
-      UTF16_IO.write(io, command, ENCODING)
+      UTF16_IO.write(io, expanded_command(args), ENCODING)
     end
     result = nil
     connection.recv(PacketIO::Type::REPLY) do |io|
