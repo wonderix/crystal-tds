e1468f83905eefbda0254ffefe3529c91f8cc552
wonderix <wonderix@googlemail.com>
2022-02-03 19:45:36 +0100

    -

--- src/tds.cr
+++ src/tds.cr
@@ -1,6 +1,33 @@
 require "db"
-require "./tds/**"
 
 module TDS
-  VERSION = "0.1.0"
+  abstract struct TypeInfo
+  end
+
+  struct Int_n < TypeInfo
+  end
+
+  class PreparedStatement < DB::Statement
+    @type_infos = [] of TypeInfo
+
+    def initialize(connection, command)
+      super(connection, command)
+    end
+
+    protected def perform_query(args : Enumerable) : DB::ResultSet
+      raise "not implemented"
+    end
+
+    def perform_exec(args : Enumerable) : DB::ExecResult
+      parameters = args.zip(@type_infos).map do |x|
+        test(x[0].as(Int32), type_info: x[1].as(TypeInfo))
+      end
+      raise "not implemented"
+    end
+
+    def test(@value : Int32, type_info : TypeInfo? = nil, @name = "") 
+    end
+  end
 end
+
+DB.open("/test").exec("CREATE DATABASE test")
--- src/tds/driver.cr
+++
@@ -1,7 +0,0 @@
-class TDS::Driver < DB::Driver
-  def build_connection(context : DB::ConnectionContext) : TDS::Connection
-    TDS::Connection.new(context)
-  end
-end
-
-DB.register_driver "tds", TDS::Driver
