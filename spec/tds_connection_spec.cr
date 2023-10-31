911c32c53ea398105ebf45c0fa42ebd23653764f
Lachlan Dowding <lachlan@permafrost.space>
2023-10-31 13:09:35 +1000

    Refactor specs to make it easier to customize test database details

--- spec/tds_connection_spec.cr
+++ spec/tds_connection_spec.cr
@@ -3,23 +3,23 @@ require "big"
 
 describe TDS::Connection do
   it "connects" do
-    DB.open URL do |db|
+    DB.open DATABASE_URI do |db|
     end
   end
-  it "raises DB::ConnectionRefused" do
-    expect_raises(DB::ConnectionRefused) do
-      DB.open "tds://sa:wrong-password@#{HOSTNAME}:1433" do |db|
+  it "raises DB::Error" do
+    expect_raises(DB::Error) do
+      DB.open "tds://#{USER}:#{PASSWORD + "XYZ"}@#{HOST}:#{PORT}" do |db|
       end
     end
   end
   it "raises DB::ConnectionRefused" do
     expect_raises(DB::ConnectionRefused) do
-      DB.open "tds://#{HOSTNAME}:5555" do |db|
+      DB.open "tds://#{HOST}:65535" do |db|
       end
     end
   end
   it "prepare statement" do
-    connection = DB.connect(URL).as(TDS::Connection)
+    connection = DB.connect(DATABASE_URI).as(TDS::Connection)
     connection.sp_prepare("@P0 int", "SELECT c1 FROM TEST WHERE c1 =  @P0 ").should eq 1
     connection.close
   end
