67262b50e8e9979b63c5db1fadb82fbe08d6720b
Lachlan Dowding <lachlan@permafrost.space>
2025-03-30 11:18:23 +1000

    Minify SQL statements before prepare/execute to mitigate 4000 character statement length limit: * remove extraneous whitespace * remove comments, single and multi-line * change parameter names to be shorter `@P$n__`, keeping the double-underscore   suffix to reduce likelihood of parameter name clashes with any declared   variables

--- src/tds/statement_methods.cr
+++ src/tds/statement_methods.cr
@@ -35,55 +35,74 @@ module TDS
 
     abstract def parameterize(args : Enumerable) : {String, Array(Parameter)}
 
-    # Replaces `?` placeholders with `@Pn` where n = 0..n placeholders in the
-    # given SQL statement, returning the updated statement, and array of
-    # parameter names and types to be passed to `sp_prepare` or `sp_executesql`
+    # Replaces `?` placeholders with `@P$n__` where n = 0..n placeholders in
+    # the given SQL statement, returning the updated statement, and array of
+    # parameter names and types for `sp_prepare` or `sp_executesql`
     def parameterize(command : String, arguments : Array(Parameter)) : {String, Array(String), Array(Parameter)}
       index, names, parameters, reordered_arguments = 0, Hash(String, String).new, Array(String).new, Array(Parameter).new(arguments.size)
       unnamed_params_found, named_params_found = false, false
+
       statement = String.build(command.size) do |string|
         scanner = StringScanner.new(command)
         until scanner.eos?
           case
-          when value = scanner.scan(/\[([^\]]|\]\])*\]/) # bracket quoted value, including escaped right brackets such as `]]`
-          when value = scanner.scan(/'([^']|'')*'/)      # single quoted value, including escaped single-quotes such as `''`
-          when value = scanner.scan(/"([^"]|"")*"/)      # double quoted value, including escaped double-quotes such as `""`
-          when value = scanner.scan(/--.*/)              # single line comment
-          when comment_start = scanner.scan(/\/\*/)      # multi-line comment
-            if comment_end = scanner.scan(/.*?\*\//m)
-              value = comment_start + comment_end
-            else
-              value = comment_start + scanner.rest
+          when value = scanner.scan(/\[([^\]]|\]\])*\]/m) # bracket quoted value, including escaped right brackets such as `]]`
+            # treat as a literal and do not modify
+          when value = scanner.scan(/'([^']|'')*'/m) # single quoted value, including escaped single-quotes such as `''`
+            # treat as a literal and do not modify
+          when value = scanner.scan(/"([^"]|"")*"/m) # double quoted value, including escaped double-quotes such as `""`
+            # treat as a literal and do not modify
+          when value = scanner.scan(/--.*/) # single line comment
+            # minify statement by removing comments
+            value = ""
+          when comment_start = scanner.scan(/\/\*/) # multi-line comment
+            unless scanner.scan(/.*?\*\//m)
               scanner.terminate
             end
+            # minify statement by removing comments
+            value = ""
           when unnamed_param = scanner.scan('?') # unnamed parameter placeholder
             index += 1
-            value = "@PARAM_CRYSTAL_TDS_$#{index}__"
+            value = "@P$#{index}__"
             parameters << value
             unnamed_params_found = true
           when named_param = scanner.scan(/\$\d+/) # named parameter placeholder
             if names.has_key? named_param
-              value = names[named_param] # named param already declared, so we can use the saved param name for it
+              # named param already declared, so we can use the saved param name for it
+              value = names[named_param]
             else
-              idx = named_param[1..].to_i # use the index in the named param to find the related argument to bind to
-              value = "@PARAM_CRYSTAL_TDS_#{named_param}__"
+              # use the index in the named param to find the related argument to bind to
+              idx = named_param[1..].to_i
+              value = "@P#{named_param}__"
               parameters << value
-              names[named_param] = value                # save the named param in case its reused later in the query
-              reordered_arguments << arguments[idx - 1] # argument values need to be reordered to match declaration order
+              # save the named param in case its reused later in the query
+              names[named_param] = value
+              # argument values need to be reordered to match declaration order
+              reordered_arguments << arguments[idx - 1]
             end
             named_params_found = true
+          when value = scanner.scan(/\s+/m) # one or more whitespace characters
+            # replace runs of whitespace characters with either a single newline
+            # if the value includes at least one newline or carriage return, or
+            # with a single space
+            if value.includes?("\n") || value.includes?("\r")
+              value = "\n"
+            else
+              value = " "
+            end
           when value = scanner.scan(/./m) # all other tokens
+            # treat as a literal and do not modify
           else
-            raise DB::Error.new("Unexpected character encountered when parsing: #{scanner.peek(1).inspect} -- query = #{command.inspect}, args = #{arguments.map {|a| a.value}.inspect}")
+            raise DB::Error.new("Unexpected character encountered when parsing: #{scanner.peek(1).inspect} -- query = #{command.inspect}, args = #{arguments.map { |a| a.value }.inspect}")
           end
           string << value
         end
       end
 
-      raise DB::Error.new("Incorrect use of parameter placeholders: using both ? and $n style placeholders within single query not allowed, choose one placeholder style and use only that style within each query -- query = #{command.inspect}, args = #{arguments.map {|a| a.value}.inspect}") if unnamed_params_found && named_params_found
-      raise DB::Error.new("Incorrect number of arguments: expected #{parameters.size}, but received #{arguments.size} -- query = #{command.inspect}, args = #{arguments.map {|a| a.value}.inspect}") if parameters.size != arguments.size
+      raise DB::Error.new("Incorrect use of parameter placeholders: using both ? and $n style placeholders within single query not allowed, choose one placeholder style and use only that style within each query -- query = #{command.inspect}, args = #{arguments.map { |a| a.value }.inspect}") if unnamed_params_found && named_params_found
+      raise DB::Error.new("Incorrect number of arguments: expected #{parameters.size}, but received #{arguments.size} -- query = #{command.inspect}, args = #{arguments.map { |a| a.value }.inspect}") if parameters.size != arguments.size
 
-      {statement, parameters.map_with_index { |value, i| "#{value} #{arguments[i].type_info.type}" }, unnamed_params_found ? arguments : reordered_arguments}
+      {statement.strip, parameters.map_with_index { |value, i| "#{value} #{arguments[i].type_info.type}" }, unnamed_params_found ? arguments : reordered_arguments}
     end
 
     def conn
